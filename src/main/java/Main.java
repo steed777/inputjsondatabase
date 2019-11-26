856a4d474f99e75139b1a8145624f57459504c6f
Senko_KL <steed777@yandex.ru>
2019-11-26 10:27:03 +0300

    Помещение в json всех таблиц из базы данных

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
+import convert.Converter;
+import convert.ResultSetConverter;
+
+import java.io.IOException;
 import java.sql.*;
 
 public class Main{
 
-    public static void main(String[] args) throws SQLException{
+    public static void main(String[] args) throws SQLException, IOException {
         ResultSetConverter convertJson = new ResultSetConverter();
        convertJson.convert();
+        Converter converter = new Converter();
+      converter.toJson(convertJson);
     }
 }
---
+++ src/main/java/convert/ResultSetConverter.java
@@ -0,0 +1,42 @@
+package convert;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class ResultSetConverter {
+    private static final String url =
+            "jdbc:firebirdsql://localhost:3050/C:/db/LAWYERS_PORTAL.FDB?lc_ctype=WIN1251&user=SYSDBA&password=masterkey";
+    public static JSONArray convert() throws SQLException, JSONException{
+        Connection connection = DriverManager.getConnection(url);
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("select * from NPA_RF");
+        ResultSet resultSet1 = statement.executeQuery("select * from SYS_ROLE");
+
+
+        JSONArray json = new JSONArray();
+        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+
+        int numColumns = resultSetMetaData.getColumnCount();
+        while (resultSet.next()) {
+            JSONObject obj = new JSONObject();
+            for (int i = 1; i < numColumns + 1; i++) {
+                String column_name = resultSetMetaData.getColumnName(i);
+                if (resultSetMetaData.getColumnType(i) == java.sql.Types.BIGINT) {
+                    obj.put(column_name, resultSet.getLong(column_name));
+                } else if (resultSetMetaData.getColumnType(i) == java.sql.Types.INTEGER) {
+                    obj.put(column_name, resultSet.getInt(column_name));
+                } else if (resultSetMetaData.getColumnType(i) == java.sql.Types.VARCHAR) {
+                    obj.put(column_name, resultSet.getString(column_name));
+                }
+            }
+            System.out.println(json);
+            json.put(obj);
+        }
+        return json;
+    }
+}
