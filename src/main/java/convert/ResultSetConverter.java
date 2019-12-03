e9e29f988bb9e6b2f62f63b8f3f93b0bd7da4e63
Senko_KL <steed777@yandex.ru>
2019-12-03 10:36:00 +0300

    Помещение в json всех таблиц из базы данных

--- src/main/java/convert/ResultSetConverter.java
+++ src/main/java/convert/ResultSetConverter.java
@@ -12,7 +12,6 @@ public class ResultSetConverter {
 
     private static final String url =
             "jdbc:firebirdsql://localhost:3050/C:/db/LAWYERS_PORTAL.FDB?lc_ctype=WIN1251&user=SYSDBA&password=masterkey";
-    private static final String dbFile = "dbFile.json";
 
     private static JSONArray json = null;
     private static JSONObject obj = null;
@@ -23,15 +22,13 @@ public class ResultSetConverter {
         Statement statement = connection.createStatement();
         ResultSet resultSet = null;
 
-        List<String> sql = new ArrayList<String>();
+        List<String> sql = new ArrayList<String>(); // добавление в список трех sql запросов
         sql.add("select * from CANSELLEDCERTIFICATES");
         sql.add("select * from CHANGE_FIO_FILTER");
         sql.add("select * from FOREIGN_LAWYERS");
 
-
-
-      // for (int j = 0; j < sql.size(); j++) {
-           resultSet = statement.executeQuery(sql.get(2));
+        for (int j = 0; j < sql.size(); j++) {// попытка поместить в json объект трех таблиц
+           resultSet = statement.executeQuery(sql.get(j));
            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
            int numColumns = resultSetMetaData.getColumnCount();
         json = new JSONArray();
@@ -52,34 +49,7 @@ public class ResultSetConverter {
                json.put(obj);
            }
            System.out.println(json);
-      // }
+       }
         return json;
     }
- /*   public List<String> convert() throws SQLException, JSONException {
-
-       sql.add("select * from CANSELLEDCERTIFICATES");
-       // convertInJson();
-        sql.add("select * from CHANGE_FIO_FILTER");
-       // convertInJson();
-        sql.add("select * from FOREIGN_LAWYERS");
-   //     convertInJson();
-       *//* sql.add("select * from LAWYERS");
-        convertInJson();*//*
-        *//*sql.add("select * from NPA_RF");
-        convertInJson();
-        sql.add("select * from SYS_ROLE");
-        convertInJson();
-        sql.add("select * from USER_ROLE");
-        convertInJson();
-        sql.add("select * from COUNTRIES");
-        convertInJson();
-        sql.add("select * from NPA_FOREIGN");
-        convertInJson();
-        sql.add("select * from REGIONS");
-        convertInJson();
-        sql.add("select * from SYS_USER");
-        convertInJson();*//*
-        return sql;
-    }*/
-//public
 }
